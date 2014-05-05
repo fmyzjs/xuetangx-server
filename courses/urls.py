5057f1d3a263e2ec7268f4b8587eb7ec30f4a4be
leon-huang <leon@njuopen.com>
2014-05-05 19:52:33 +0800

    app courses: urls

---
+++ courses/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, url
+
+from courses import views
+
+urlpatterns = patterns('',
+    url(r'^selected/$', views.selected),
+    url(r'^upcoming/$', views.upcoming),
+    url(r'^current/$', views.current),
+    url(r'^past/$', views.past),
+    url(r'^search/$', views.search),
+    url(r'^about/$', views.about),
+    url(r'^info/$', views.info),
+    url(r'^ware/$', views.ware),
+)
+
+
+
+
+
+
+
+
+

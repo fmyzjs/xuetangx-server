591cc232c009e0c89fb2a93a46499567bbb0d7d0
leon-huang <leon@njuopen.com>
2014-05-05 19:47:10 +0800

    app student: views stub

--- student/views.py
+++ student/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def verify(request):
+    return HttpResponse("You're at the student verify.")
+
+def info(request):
+    return HttpResponse("You're at the student info.")

b6c61347c93935ee901fc58747ad4eb651ca0a97
leon-huang <leon@njuopen.com>
2014-05-06 17:44:49 +0800

    stubs for app courses

--- courses/views.py
+++ courses/views.py
@@ -2,33 +2,79 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def verify(request):
-    return HttpResponse("You're at the student verify.")
-
-def info(request):
-    return HttpResponse("You're at the student info.")
-
 def selected(request):
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses selected.")
 
 def upcoming(request):
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses upcoming.")
 
 def current(request):
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses current.")
 
 def past(request):
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses past.")
 
 def search(request):
+    try:
+        key = request.GET['key']
+        category = request.GET['category']
+        started = request.GET['started'] # bool
+        hasTA = request.GET['hasTA'] # bool
+        # TODO: check whether category in the list
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses search.")
 
 def about(request):
+    try:
+        url = request.GET['url']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses about.")
 
 def info(request):
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+        url = request.GET['url']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses info.")
 
 def ware(request):
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+        url = request.GET['url']
+    except (ValueError, KeyError):
+        return HttpResponse(utils.template.invalid_request())
+
     return HttpResponse("You're at the courses ware.")
 
